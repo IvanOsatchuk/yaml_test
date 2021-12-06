a08bdf0667ddd30a993a7b0d29fad4c629d023c2
IvanOsatchuk <38212423+IvanOsatchuk@users.noreply.github.com>
2021-12-06 16:55:27 -0300

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -9,10 +9,8 @@ def func_labs_migration_pipeline(env):
     process = subprocess.run(
         shlex.split("""gcloud projects describe rapid-strength-333817"""), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
     )
-    instance_output, err = process.communicate()
-    if err:
-        print(err)
-    print(instance_output.decode("UTF-8").splitlines())
+   
+    print(process)
     
     '''
     print('ENTROU')
