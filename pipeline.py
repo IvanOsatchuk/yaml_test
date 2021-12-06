502cb5bd36b592cc5a1c4792a48f1c5b35d8b9dd
IvanOsatchuk <38212423+IvanOsatchuk@users.noreply.github.com>
2021-12-06 16:47:52 -0300

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -3,7 +3,7 @@ import executor
 import subprocess
 import shlex
 
-def func_labs_migration_pipeline(env, log_on_cbuild=False, return_first_line=True):
+def func_labs_migration_pipeline(env):
     
     process = subprocess.Popen(
         shlex.split("gcloud projects describe rapid-strength-333817"), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
@@ -11,12 +11,6 @@ def func_labs_migration_pipeline(env, log_on_cbuild=False, return_first_line=Tru
     instance_output, err = process.communicate()
     if err:
         print(err)
-    if log_on_cbuild:
-        for line in instance_output.decode("UTF-8").splitlines():
-            print(line)
-    if return_first_line:
-        print("Return only the first line")
-        print(instance_output.decode("UTF-8").splitlines()[0])
     print(instance_output.decode("UTF-8").splitlines())
     
     '''
