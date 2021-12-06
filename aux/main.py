fd76d38c20e8c92ed2f34f72a7a602b5461b5e40
IvanOsatchuk <38212423+IvanOsatchuk@users.noreply.github.com>
2021-12-06 10:52:42 -0300

    Update main.py

--- aux/main.py
+++ aux/main.py
@@ -1,9 +1,36 @@
-import yaml
+def config_parse():
+    print("Start Data Fusion CICD")
+    parser = argparse.ArgumentParser(description="Script to execute Data Fusion CICD.")
 
-vars = yaml.full_load(open("vars.yaml"))
+    parser.add_argument(
+        "--env",
+        type=str,
+        default="dev",
+        dest="env",
+        help="Name of the project for Cloud Data Fusion. Default to: data-tools-developer",
+    )
+    return parser
 
-print(vars.get('sensitive'))
+def config_dataops_parse(subparsers):
+    dataops_parser = subparsers.add_parser("dataops", help="Execute dataops tests")
+    dataops_parser.set_defaults(func=dataops_func)
 
-print(vars.get('functions'))
 
-print("hello Python")
+def dataops_func(args):
+    cdf_project = args.cdf_project
+    pipeline.func_labs_migration_pipeline(
+        env
+    )
+
+
+if __name__ == "__main__":
+    parser = config_parse()
+    subparsers = parser.add_subparsers(
+        dest="subparser_name", description="Possible Statements to execute"
+    )
+
+    config_dataops_parse(subparsers)
+
+    args = parser.parse_args()
+    print("Started args: ", args)
+    dataops_func(args)
