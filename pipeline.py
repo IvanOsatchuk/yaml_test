ee103b8517d13dffdf3331055f580850c8ba509e
IvanOsatchuk <38212423+IvanOsatchuk@users.noreply.github.com>
2021-12-06 17:51:45 -0300

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -6,22 +6,5 @@ import os
 
 def func_labs_migration_pipeline(env):
     
-    subprocess.run(["gcloud", "projects", "describe", "rapid-strength-333817"], shell=True)
-    
-    
-    process = subprocess.run(
-        shlex.split("""gcloud auth print-access-token"""), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
-    )
-   
-    print(process)
-    
-    '''
-    print('ENTROU')
-    output = subprocess.run(shlex.split("gcloud projects describe rapid-strength-333817"),  shell=True)
-    
-    print(output)
-
-    print('RESULTADO')
-    print(result)
-    #return result
-    '''
+    print(env)
+    print('ENTROOOOOOOOOOOU')
