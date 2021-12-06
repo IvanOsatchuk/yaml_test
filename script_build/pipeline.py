217c44f547eea1d3656c6d81146e3f5566c24da6
IvanOsatchuk <38212423+IvanOsatchuk@users.noreply.github.com>
2021-12-06 15:31:09 -0300

    Update pipeline.py

--- script_build/pipeline.py
+++ script_build/pipeline.py
@@ -4,13 +4,6 @@ import shlex
 
 def func_labs_migration_pipeline(env):
   
-  instance_output = subprocess.run(
-        shlex.split("gcloud projects describe rapid-strength-333817"),
-        check=True,
-        stdout=subprocess.DEVNULL,
-        stderr=subprocess.STDOUT,
-    )
-  
-  print(instance_output)
+
   
   print(env)
