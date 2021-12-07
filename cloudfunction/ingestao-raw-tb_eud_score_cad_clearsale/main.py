85d61b5b13af219fda9175b968a556a2dd62c31b
IvanOsatchuk <38212423+IvanOsatchuk@users.noreply.github.com>
2021-12-07 20:37:42 -0300

    Update main.py

--- cloudfunction/ingestao-raw-tb_eud_score_cad_clearsale/main.py
+++ cloudfunction/ingestao-raw-tb_eud_score_cad_clearsale/main.py
@@ -1,10 +1,16 @@
-/**
- * Responds to any HTTP request.
- *
- * @param {!express:Request} req HTTP request context.
- * @param {!express:Response} res HTTP response context.
- */
-exports.helloWorld = (req, res) => {
-  let message = req.query.message || req.body.message || 'Hello World!';
-  res.status(200).send(message);
-};
+def hello_world(request):
+    """Responds to any HTTP request.
+    Args:
+        request (flask.Request): HTTP request object.
+    Returns:
+        The response text or any set of values that can be turned into a
+        Response object using
+        `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
+    """
+    request_json = request.get_json()
+    if request.args and 'message' in request.args:
+        return request.args.get('message')
+    elif request_json and 'message' in request_json:
+        return request_json['message']
+    else:
+        return f'Hello World!'
