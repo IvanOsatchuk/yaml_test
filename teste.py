c2edf4cdd6ff76cd2a908b5679796ec2acc2d81a
IvanOsatchuk <38212423+IvanOsatchuk@users.noreply.github.com>
2021-12-07 17:27:58 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,18 +1,24 @@
 import yaml
 import sys
+import subprocess
 
 print(sys.argv)
+
+
 
 file = yaml.full_load(open('vars.yaml'))
 
 
-with open('./workspace/validate_sensitive.txt','w') as f:
-    f.write(str(file['IsSensitive']).lower())
+#with open('./workspace/validate_sensitive.txt','w') as f:
+#    f.write(str(file['IsSensitive']).lower())
 
-with open('./workspace/functions_name.txt','w') as f:
-    f.write(' '.join(file['Functions']))
+#with open('./workspace/functions_name.txt','w') as f:
+#    f.write(' '.join(file['Functions']))
 
 
+subprocess.call('echo ' + ' '.join(file['Functions']) + '/workspace/is_sentive.txt', shell=True)
+    
+    
 if 'IsSensitive' not in file:
     raise Exception("[ERROR] IsSensitive fields not found")
 if 'Functions' not in file:
