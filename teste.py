c171344d6fd430f4ecd3ceeabcbfee88393fe2b6
IvanOsatchuk <38212423+IvanOsatchuk@users.noreply.github.com>
2021-12-07 18:22:16 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -55,6 +55,7 @@ if __name__ == "__main__":
         
     for func in file['Functions']:
         print(f"teste {func}")
+        subprocess.call('echo "teste"', shell=True)
         subprocess.call(f"gcloud functions deploy {func} --region=us-central1 --project={path_project} --source=./cloudfunction/{func} --trigger-http --entry-point=main --runtime=python39 --memory=2048MB --timeout=540 --set-env-vars=ENVIRONMENT={env}", shell=True)
 
 
