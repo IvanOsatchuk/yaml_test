d4e4391d687ba5f9a4a3113bc2f7ee4551941f26
IvanOsatchuk <38212423+IvanOsatchuk@users.noreply.github.com>
2021-12-07 18:10:50 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -3,6 +3,15 @@ import sys
 import subprocess
 import argparse
 
+def validate():
+    file = yaml.full_load(open('vars.yaml'))
+    if 'IsSensitive' not in file:
+        raise Exception("[ERROR] IsSensitive fields not found")
+    if 'Functions' not in file:
+        raise Exception("[ERROR] Functions fields not found")
+    if file['IsSensitive'] not in [True, False]:
+        raise Exception("[ERROR] IsSensitive invalid value")
+
 def config_parse():
     print("Start Data Fusion CICD")
     parser = argparse.ArgumentParser(description="ENV vars")
@@ -24,38 +33,29 @@ def config_parse():
     )
     return parser
 
-
-parser = config_parse()
-args = parser.parse_args()
-print("Started args: ", args)
-
-env = args.env
-sensitive_path = args.sensitive_path
-non_sensitive_path = args.non_sensitive_path
-path_project = ''
-
-file = yaml.full_load(open('vars.yaml'))
-
-if file['IsSensitive'] == True:
-    path_project = sensitive_path
-else:
-    path_project = non_sensitive_path
+if __name__ == "__main__":
     
+    validate()
 
-for func in file['Functions']:
-    subprocess.call(f"echo gcloud functions deploy $func --region=us-central1 --project={path_project} --source=./cloudfunction/{func} --trigger-http --entry-point=main --runtime=python39 --memory=2048MB --timeout=540 --set-env-vars=ENVIRONMENT=${env}", shell=True)
+    parser = config_parse()
+    args = parser.parse_args()
+    print("Started args: ", args)
+
+    env = args.env
+    sensitive_path = args.sensitive_path
+    non_sensitive_path = args.non_sensitive_path
+    path_project = ''
+
+    if file['IsSensitive'] == True:
+        path_project = sensitive_path
+    else:
+        path_project = non_sensitive_path
+        
+    for func in file['Functions']:
+        print(f"teste {func}")
+        subprocess.call(f"echo gcloud functions deploy $func --region=us-central1 --project={path_project} --source=./cloudfunction/{func} --trigger-http --entry-point=main --runtime=python39 --memory=2048MB --timeout=540 --set-env-vars=ENVIRONMENT=${env}", shell=True)
 
 
-#print(" ".join(file['Functions']))
+   
 
-#subprocess.call('echo ' + str(file['IsSensitive']).lower() + '> /workspace/validate_sensitive.txt', shell=True)
-
-#subprocess.call('echo ' + "".join(file['Functions']) + '> /workspace/functions_name.txt', shell=True)
     
-    
-if 'IsSensitive' not in file:
-    raise Exception("[ERROR] IsSensitive fields not found")
-if 'Functions' not in file:
-    raise Exception("[ERROR] Functions fields not found")
-if file['IsSensitive'] not in [True, False]:
-    raise Exception("[ERROR] IsSensitive invalid value")
