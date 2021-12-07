61c3dba59dc6a4f5e691101b0832e844a4d8098a
IvanOsatchuk <38212423+IvanOsatchuk@users.noreply.github.com>
2021-12-07 17:17:37 -0300

    Create teste.py

---
+++ teste.py
@@ -0,0 +1,18 @@
+import yaml
+
+file = yaml.full_load(open('vars.yaml'))
+
+
+with open('./workspace/validate_sensitive.txt','w') as f:
+    f.write(str(file['IsSensitive']).lower())
+
+with open('./workspace/functions_name.txt','w') as f:
+    f.write(' '.join(file['Functions']))
+
+
+if 'IsSensitive' not in file:
+    raise Exception("[ERROR] IsSensitive fields not found")
+if 'Functions' not in file:
+    raise Exception("[ERROR] Functions fields not found")
+if file['IsSensitive'] not in [True, False]:
+    raise Exception("[ERROR] IsSensitive invalid value")
