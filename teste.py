0e5f494ded7acfe39ac14fdc0c1e1de3ee32cf99
IvanOsatchuk <38212423+IvanOsatchuk@users.noreply.github.com>
2021-12-07 19:27:10 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -63,13 +63,7 @@ if __name__ == "__main__":
     else:
         path_project = non_sensitive_path
     
-    p = Popen("""echo teste""", shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
-    output, err = p.communicate(b"input data that is passed to subprocess' stdin")
-    rc = p.returncode
     
-    print(rc)
-    
-    print(output)
     
     
         
@@ -77,6 +71,13 @@ if __name__ == "__main__":
     for func in file['Functions']:
         print(f"teste {func}")
         #call_gcloud(func, path_project, env)
+        p = Popen("""gcloud functions deploy {func} --region=us-central1 --project={path_project} --source=./cloudfunction/{func} --trigger-http --entry-point=main --runtime=python39 --memory=2048MB --timeout=540 --set-env-vars=ENVIRONMENT={env}""", shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        output, err = p.communicate(b"input data that is passed to subprocess' stdin")
+        rc = p.returncode
+    
+        print(rc)
+    
+        print(output)
         
         
         
