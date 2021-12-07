8da02be8ffa2ae73ab2eae212bf57f64eb32266b
IvanOsatchuk <38212423+IvanOsatchuk@users.noreply.github.com>
2021-12-07 20:37:10 -0300

    Update main.py

--- cloudfunction/ingestao-raw-tb_eud_score_cad_clearsale/main.py
+++ cloudfunction/ingestao-raw-tb_eud_score_cad_clearsale/main.py
@@ -1,1 +1,10 @@
-print('primeira funcao')
+/**
+ * Responds to any HTTP request.
+ *
+ * @param {!express:Request} req HTTP request context.
+ * @param {!express:Response} res HTTP response context.
+ */
+exports.helloWorld = (req, res) => {
+  let message = req.query.message || req.body.message || 'Hello World!';
+  res.status(200).send(message);
+};
